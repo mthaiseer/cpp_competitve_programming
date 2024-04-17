13bba06579d207a75460e5188aae1bea5329bcd8
Mohamed Thaiseer <mohamed.thaiseer@gmail.com>
2024-04-17 19:50:53 +0300

    D

---
+++ binary_search/001_lower_upper_bound/code.cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int lower(vector<int>& arr, int l, int n){
+
+    int start  = 0;
+    int end = n-1;
+    int ans = -1;
+    while( start <= end ){
+
+        int mid = (start + end) /2;
+
+        if( arr[mid] > l){
+            ans = mid;
+             end = mid -1;
+        }else{
+            start = mid +1;
+        }
+    }
+
+    return ans;
+}
+
+int upper(vector<int>& arr, int r, int n){
+
+    int start  = 0;
+    int end = n-1;
+    int ans = -1;
+    while( start <= end ){
+
+        int mid = (start + end) /2;
+
+        debug(mid);
+      
+        if( arr[mid] <= r){
+
+            ans = mid;
+            start = mid +1;
+
+        }else{
+          
+            end = mid-1;
+        }
+
+    }
+
+    return ans;
+}
+
+
+int solve( vector<int>& arr, int n, int l, int r){
+  
+  debug(r);
+    
+    int right = upper(arr, r, n);
+    int left = lower(arr, l, n);
+
+    if( right  == -1 || left  == -1) return -1;
+   
+    return (right - left)+1;
+}
+
+int main() {
+
+    fastio();
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+
+    int n, l, r;
+    cin>> n>> l>> r;
+    vector<int> arr(n);
+
+    for(int i=0; i< n; i++){
+        cin>>arr[i];
+    }
+
+    int res =  solve(arr, n, l , r);
+
+    cout<< res <<"\n";
+   
+}
