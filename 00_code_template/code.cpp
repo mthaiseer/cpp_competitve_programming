db4441813258ba9f19c3adbd736dac570399944b
Mohamed Thaiseer <mohamed.thaiseer@gmail.com>
2024-04-22 20:57:54 +0300

    Update code.cpp

--- 00_code_template/code.cpp
+++ 00_code_template/code.cpp
@@ -31,6 +31,8 @@ typedef long double lld;
 #define debug(x)
 #endif
 
+#define println( t)  _printnl(t);
+
 void _print(ll t) {cerr << t;}
 void _print(int t) {cerr << t;}
 void _print(string t) {cerr << t;}
@@ -38,6 +40,14 @@ void _print(char t) {cerr << t;}
 void _print(lld t) {cerr << t;}
 void _print(double t) {cerr << t;}
 void _print(ull t) {cerr << t;}
+
+void _printnl(ll t) {cout<< t<< "\n";}
+void _printnl(int t) {cout<< t<< "\n";}
+void _printnl(string t) {cout<< t<< "\n";}
+void _printnl(char t) {cout<< t<< "\n";}
+void _printnl(lld t) {cout<< t<< "\n";}
+void _printnl(double t) {cout<< t<< "\n";}
+void _printnl(ull t) {cout<< t<< "\n";}
 
 template <class T, class V> void _print(pair <T, V> p);
 template <class T> void _print(vector <T> v);
@@ -48,7 +58,8 @@ template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
 
 
 int main() {
@@ -58,6 +69,9 @@ int main() {
         freopen("Error.txt", "w", stderr);
     #endif
 
-   
-   
+    
+
+    println("dsds")
+
+	
 }
